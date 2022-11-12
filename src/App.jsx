4bf78b6fab8f0fd72bce7ce354315b7471dab369
Bryan Hatcher <bryanhatcher34@gmail.com>
2022-11-12 17:45:07 -0500

    Keywords, Dropdown Menu, and Zoom based on location done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState, useEffect } from "react";
-//import Data from "./DummyData.js";
 import axios from "axios";
 import Markers from "./components/Markers";
+import NavBar from "./components/NavBar";
+
 function App() {
   const [mapData, setMapData] = useState([]);
-  const viewport = {
+  const [viewPort, setViewPort] = useState({
     latitude: 38.483964,
     longitude: -94.681538,
     zoom: 4,
-  };
-  const search = "Junior React Developer"
-  const perPage = 50
-  const url = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=2ab7411c&app_key=5eb49153dbc20fd4904557e94acbf1a3&title_only=React"
-  //`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=2ab7411c&app_key=5eb49153dbc20fd4904557e94acbf1a3&results_per_page=50&title_only=ReactDeveloper%20&where=Connecticut`
-  //`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=2ab7411c&app_key=5eb49153dbc20fd4904557e94acbf1a3&results_per_page=${perPage}&title_only=${search}`;
+  });
+  const [keyword, setKeyword] = useState("Junior Developer");
+  const [location, setLocation] = useState("USA");
+  const perPage = 1000;
+  const url = `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=2ab7411c&app_key=5eb49153dbc20fd4904557e94acbf1a3&results_per_page=${perPage}&what=${keyword}&where=${location}`;
+
+  console.log(viewPort)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,21 +27,22 @@ function App() {
     fetchData();
   }, [url]);
 
-  console.log(mapData)
 
   return (
-    <div>
-      <div className="bg-black text-white">Hello World</div>
+    <div className="flex">
       <Map
+        {...viewPort}
+        onMove={evt => setViewPort(evt.viewPort)}
         mapboxAccessToken="pk.eyJ1Ijoicm9ib2dob3N0NTgyIiwiYSI6ImNsYWNvcnl6MjA2Z240MXF2NXl1OXR1d3gifQ.pcL3VAIzYvsPXqUIXFm8wA"
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/dark-v10"
-        initialViewState={viewport}
+        
       >
         {mapData.map((item) => {
           return <Markers key={item?.id} item={item} />;
         })}
       </Map>
+      <NavBar setKeyword = {setKeyword} setLocation = {setLocation} viewPort = {viewPort} setViewPort = {setViewPort}/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,84 @@
+import React, {useState } from "react";
+import { BiChevronDown } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
+import States from '../StateData.js'
+
+function NavBar({ setKeyword, setLocation, setViewPort, viewPort}) {
+  const [inputValue, setInputValue] = useState("");
+  const [selected, setSelected] = useState("");
+  const [open, setOpen] = useState(false);
+
+
+  return (
+    <div className="flex flex-col justify-start items-center bg-[#191a1a] gap-6 p-4">
+      <input
+        className="border-2 border-black py-2 px-4 my-1 rounded-xl w-[250px]"
+        placeholder="Job Keyword(s)..."
+        type="text"
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+
+        <div
+          onClick={() => setOpen(!open)}
+          className={`bg-white w-[250px] p-2 flex items-center justify-between rounded-xl ${
+            !selected && "text-gray-700"
+          }`}
+        >
+          {selected
+            ? selected?.length > 25
+              ? selected?.substring(0, 25) + "..."
+              : selected
+            : "Select State"}
+          <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
+        </div>
+        <ul
+          className={`bg-white mt-2 overflow-y-auto ${
+            open ? "max-h-60" : "max-h-0"
+          } `}
+        >
+          <div className="flex items-center px-2 sticky top-0 bg-white">
+            <AiOutlineSearch size={18} className="text-gray-700" />
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value.toLowerCase())}
+              placeholder="Enter State name"
+              className="placeholder:text-gray-700 p-2 outline-none"
+            />
+          </div>
+          {States?.map((item) => (
+            <li
+              key={item?.state}
+              className={`p-2 text-sm hover:bg-sky-600 hover:text-white
+            ${
+                item?.state.toLowerCase() === selected?.toLowerCase() &&
+              "bg-sky-600 text-white"
+            }
+            ${
+                item?.state.toLowerCase().startsWith(inputValue)
+                ? "block"
+                : "hidden"
+            }`}
+              onClick={() => {
+                if (item?.state.toLowerCase() !== selected.toLowerCase()) {
+                  setLocation(item?.state);
+                  {item?.state === "USA" ? 
+                  (setViewPort({...viewPort, latitude: item.latitude, longitude: item.longitude, zoom: 4})) : 
+                  (setViewPort({...viewPort, latitude: item.latitude, longitude: item.longitude, zoom: 6})) }
+                  setSelected(item?.state);
+                  setOpen(false);
+                  setInputValue("")
+
+                }
+              }}
+            >
+              {item?.state}
+            </li>
+          ))}
+        </ul>
+
+    </div>
+  );
+}
+
+export default NavBar;
